2fd70d8f593f88d21deb342de4b9c5c59eec15fc
Daniel <daniel.plotnicov00@e-uvt.ro>
2023-05-01 15:12:14 +0300

    lil upate

--- app/src/main/java/com/my/lab1/AddInfo.kt
+++ app/src/main/java/com/my/lab1/AddInfo.kt
@@ -1,13 +1,14 @@
 package com.my.lab1
 
-import android.content.ContentValues
+import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.File
@@ -15,62 +16,72 @@ import java.io.FileOutputStream
 
 class AddInfo : AppCompatActivity() {
 
+    private lateinit var fileNameEditText: EditText
+    private lateinit var fileContentEditText: EditText
+    private lateinit var confirmButton: Button
+
+    private val storagePermissionCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addinfo)
 
-        val fileName = findViewById<EditText>(R.id.file_name)
-        val fileContent = findViewById<EditText>(R.id.file_content)
-        val confirmButton = findViewById<Button>(R.id.confirm_button)
-
-        // Get the content resolver
-        val contentResolver = applicationContext.contentResolver
-
-        // Set the MIME type for the text file
-        val mimeType = "text/plain"
-
-        // Create the ContentValues object with the file's metadata
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
-            put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
-        }
-
-        // Insert the file metadata into the MediaStore and get the file's URI
-        val uri = contentResolver.insert(MediaStore.Files.getContentUri("external"), contentValues)
-
-        // Open an output stream to write the file contents to the MediaStore
-        val outputStream = contentResolver.openOutputStream(uri!!)
-        outputStream?.write(fileContent.toByteArray())
-        outputStream?.close()
-
-        // Print the file's URI
-        Log.d("MainActivity", "File saved at: $uri")
-
+        fileNameEditText = findViewById(R.id.file_name)
+        fileContentEditText = findViewById(R.id.file_content)
+        confirmButton = findViewById(R.id.confirm_button)
 
         confirmButton.setOnClickListener {
-            val fileNameString = fileName.text.toString()
-            val fileContentString = fileContent.text.toString()
-
-            val filePath = Environment.getExternalStorageDirectory().toString() + File.separator + fileNameString
-            val file = File(filePath)
-            val fos = FileOutputStream(file)
-            fos.write(fileContentString.toByteArray())
-            fos.close()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSION_REQUEST_STORAGE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, do your work
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    storagePermissionCode
+                )
             } else {
-                // Permission denied, show a message or disable features that require permission
+                createFile()
             }
         }
     }
-    companion object {
-        private const val PERMISSION_REQUEST_STORAGE = 1000
+
+    private fun createFile() {
+        val fileName = fileNameEditText.text.toString()
+        val fileContent = fileContentEditText.text.toString()
+
+        val externalStorageState = Environment.getExternalStorageState()
+        if (externalStorageState == Environment.MEDIA_MOUNTED) {
+            val directory = getExternalFilesDir(null)
+            val file = File(directory, fileName)
+            try {
+                val fileOutputStream = FileOutputStream(file)
+                fileOutputStream.write(fileContent.toByteArray())
+                fileOutputStream.close()
+                Toast.makeText(this, "File saved to $directory/$fileName", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        } else {
+            Toast.makeText(this, "External storage is not mounted", Toast.LENGTH_SHORT).show()
+        }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == storagePermissionCode && grantResults.isNotEmpty()
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            createFile()
+        } else {
+            Toast.makeText(
+                this,
+                "Storage permission is required to create the file",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 }
